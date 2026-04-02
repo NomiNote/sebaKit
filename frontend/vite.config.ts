0d385c78d17cca5f4e73bdd8b2cb7026ac980023
Tahsin Ahmed <tahsinqp10@gmail.com>
2026-04-02 21:33:49 +0600

    feat: initialize project structure with ESP32 firmware, and frontend configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // or '0.0.0.0'
+    allowedHosts: ['arc.local'],
     port: 5173,
     proxy: {
       '/api': 'http://127.0.0.1:8080',
